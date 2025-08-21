7378ddb9b9fd58ca1551c433d2f0c72e4cb4d70e
Kibe <kiberichard.kr@gmail.com>
2025-08-21 18:58:20 +0300

    .

--- lib/DB/connectDB.ts
+++ lib/DB/connectDB.ts
@@ -8,21 +8,32 @@ if (!MONGODB_URI) {
   );
 }
 
-/* Global is used here to maintain a cached connection across hot reloads in dev */
-let cached = (global as any).mongoose;
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
+  if (cached!.conn) {
+    return cached!.conn;
   }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
+  if (!cached!.promise) {
+    cached!.promise = mongoose
+      .connect(MONGODB_URI)
+      .then((mongoose) => mongoose);
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
